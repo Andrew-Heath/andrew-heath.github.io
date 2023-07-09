d97309113f176962df81e63298e4e16981662b32
aheath42 <aheath42@gmail.com>
2023-07-09 00:20:04 -0700

    add consts file

---
+++ src/app/_models/consts.ts
@@ -0,0 +1,12 @@
+// Generic Consts
+export const NONE = 'NONE'
+
+// Special Ability Consts
+export const DEFLECT = 'DEFLECT'
+export const ABSORPTION = 'ABSORPTION'
+export const COVER = 'COVER'
+
+// Special Attack Consts
+export const TWF = 'TWF'
+export const POWER_ATTACK = 'POWER_ATTACK'
+export const TWIN_FANG = 'TWIN_FANG'
