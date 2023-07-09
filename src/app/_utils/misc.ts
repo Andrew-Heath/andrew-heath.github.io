a822abf7b67a99eeab34fc47e4a657294d984644
aheath42 <aheath42@gmail.com>
2023-07-09 01:50:29 -0700

    add median helper

--- src/app/_utils/misc.ts
+++ src/app/_utils/misc.ts
@@ -3,3 +3,5 @@ import { v5 } from 'uuid'
 const NAMESPACE = "b80d5497-5bc8-40a7-aef0-4ab829c7c784" 
 
 export const uuid = (text: string) => v5(text, NAMESPACE)
+
+export const median = (min: number, val: number, max: number): number => Math.min(max, (Math.max(val, min)))
